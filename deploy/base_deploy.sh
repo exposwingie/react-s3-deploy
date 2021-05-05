d1f82b969a01b0ee4ec8888a4a2ed5dcda3dd43e
Vanya <vanyadnip@gmail.com>
2021-05-05 17:21:08 +0300

    Refactord bash scripts

--- deploy/base_deploy.sh
+++ deploy/base_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yarn build && \
-aws s3 rm s3://$BUCKET_NAME --recursive && \
-aws s3 sync build/ s3://$BUCKET_NAME --acl public-read
+aws s3 rm s3://$FULL_BUCKET_NAME --recursive && \
+aws s3 sync build/ s3://$FULL_BUCKET_NAME --acl public-read
